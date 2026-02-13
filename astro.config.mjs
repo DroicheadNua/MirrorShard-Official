682e39ce0ef6a6f7aff65321b49048795cce06f7
DroicheadNua <219840423+DroicheadNua@users.noreply.github.com>
2026-02-14 01:50:33 +0900

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // 自分のGitHub PagesのURL（ドメイン）
+  site: 'https://droicheadnua.github.io',
+  // リポジトリ名（サブパス）
+  base: '/MirrorShard_2', // これがないとCSSや画像のリンクが切れます
+});
